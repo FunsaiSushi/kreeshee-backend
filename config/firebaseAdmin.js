9c2c0a7705f2d0422510481165b3a017c10037c6
Muaz-Hasan <moaz77hasan@gmail.com>
2024-08-20 00:44:49 +0600

    firebaseAdmin filepath updated for render

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "./kreeshee-service.json" assert { type: "json" };
+import serviceAccount from "/etc/secrets/kreeshee-service.json" assert { type: "json" };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

e3fb4db822508b05a35c6e489069724f4d50a81a
Muaz-Hasan <moaz77hasan@gmail.com>
2024-09-11 22:45:19 +0600

    firebase admin fixed again

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
-import serviceAccount from "./kreeshee-service.json" assert { type: "json" };
-// import serviceAccount from "/etc/secrets/kreeshee-service.json" assert { type: "json" };
+// import serviceAccount from "./kreeshee-service.json" assert { type: "json" };
+import serviceAccount from "/etc/secrets/kreeshee-service.json" assert { type: "json" };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
